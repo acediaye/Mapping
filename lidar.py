79dbaa1080852efebcb3b5bd112085f930b7f3da
Andy <ayeh616@gmail.com>
2021-07-08 18:20:10 -0400

    fixed blank bug

--- lidar.py
+++ lidar.py
@@ -4,14 +4,13 @@ import numpy as np
 # colors
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-GREEN = (0, 255, 0)
+# GREEN = (0, 255, 0)
 
 
 class Lidar(object):
     def __init__(self, sensor_range, mymap):
         self.sensor_range = sensor_range
         self.mymap = mymap
-        # print(mymap.get_size())
         self.width, self.height = mymap.get_size()
         self.obstacles = []
 
@@ -29,7 +28,7 @@ class Lidar(object):
             for i in range(0, 101):  # 0->100
                 u = i / 100  # 0->1
                 x = int(x1*(1-u) + x2*(u))  # x1->x2
-                y = int(y1*(1-u) + y2*(u))
+                y = int(y1*(1-u) + y2*(u))  # y1->y2
                 if 0 < x < self.width and 0 < y < self.height:
                     color = self.mymap.get_at((x, y))
                     if (color[0], color[1], color[2]) == (BLACK):
@@ -52,7 +51,8 @@ class World(object):
         self.map_height, self.map_width = map_size
         self.mymap = pygame.display.set_mode((self.map_width, self.map_height))
         self.mymap.blit(self.floor_plan, (0, 0))
-        self.map_copy = self.mymap.copy()
+        self.map_blank = self.mymap.copy()
+        self.map_data = self.mymap.copy()
 
     def obstacle_pos(self, sensor_pos, distance, angle) -> tuple:
         x = int(sensor_pos[0] + distance*np.cos(angle))
@@ -68,22 +68,21 @@ class World(object):
                     self.point_cloud.append(point)
 
     def show_data(self):
+        self.infomap = self.mymap.copy()
         for point in self.point_cloud:
-            self.map_copy.set_at((point[0], point[1]), RED)
+            # self.map_data.set_at((point[0], point[1]), RED)
+            self.infomap.set_at((point[0], point[1]), RED)
 
 
 pygame.init()
 pygame.display.set_caption('lidar')
 world = World((600, 1200))
 
-# world.original = world.mymap.copy()
-# world.mymap.fill(BLACK)
+world.original = world.mymap.copy()
+lidar = Lidar(200, world.original)
+world.mymap.fill(BLACK)
+# world.map.fill(BLACK)
 world.infomap = world.mymap.copy()
-world.infomap.fill(BLACK)
-
-lidar = Lidar(200, world.mymap)
-# world.map_copy.fill(BLACK)
-
 
 run = True
 sensor_on = False
@@ -101,7 +100,8 @@ while run:
         sensor_data = lidar.find_obstacles(position)
         world.store_data(sensor_data)
         world.show_data()
-    world.mymap.blit(world.map_copy, (0, 0))
-    # world.mymap.blit(world.infomap, (0, 0))
+    world.mymap.blit(world.infomap, (0, 0))
+    # world.mymap.blit(world.map_blank, (0, 0))
+    # world.mymap.blit(world.map_data, (0, 0))
 
     pygame.display.update()
